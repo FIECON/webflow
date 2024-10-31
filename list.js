2bf1e3810bdf7265ff237fe7be8149d868d5b885
Damian Kosinski <damian.kosinski@fiecon.com>
2024-10-31 09:30:00 +0000

    added onclick events

--- list.js
+++ list.js
@@ -1,13 +1,11 @@
 function filterDatesByYear(dateSelector, year) {
-    // Find all elements matching the date selector using jQuery
+    $('.collection-item').show();
+
     $(dateSelector).each(function() {
-        // Get the date text and parse it
         const dateText = $(this).text();
         const fullYear = parseInt(dateText.split(', ')[1].trim(), 10);
         
-        // If the year doesn't match the filter year
         if (fullYear !== year) {
-            // Find the closest parent with class 'collection-item' and hide it
             $(this).closest('.collection-item').hide();
         }
     });
@@ -15,5 +13,12 @@ function filterDatesByYear(dateSelector, year) {
 
 document.addEventListener("DOMContentLoaded", function() {
     const currentYear = new Date().getFullYear();
-    // filterDatesByYear(".date", currentYear);
+    filterDatesByYear(".date", currentYear);
+
+    $('.year').on('click', function() {
+        const selectedYear = parseInt($(this).attr('id'), 10);
+        $('.year').removeClass('active');
+        $(this).addClass('active');
+        filterDatesByYear('.date', selectedYear);
+    });
 });
